8f4d0b0a165d6ce9c47affd5abc19f3fbcf813d7
Leone Parise <leone.parise@gmail.com>
2016-09-29 14:55:59 -0300

    Initial Commit

---
+++ GooglePlaces/GooglePlaces.h
@@ -0,0 +1,19 @@
+//
+//  GooglePlaces.h
+//  GooglePlaces
+//
+//  Created by Leone Parise Vieira da Silva on 29/09/16.
+//  Copyright © 2016 com.google. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GooglePlaces.
+FOUNDATION_EXPORT double GooglePlacesVersionNumber;
+
+//! Project version string for GooglePlaces.
+FOUNDATION_EXPORT const unsigned char GooglePlacesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GooglePlaces/PublicHeader.h>
+
+
